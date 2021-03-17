9b8c3a83079800b04f4b8e395235f62919bc5041
Brendan Asselstine <mail.asselstine@gmail.com>
2021-03-17 13:55:01 -0700

    Published 1.0.1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
+require('hardhat-abi-exporter')
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.4.22",
+  abiExporter: {
+    path: './abis',
+    clear: true,
+    flat: true
+  }
 };
